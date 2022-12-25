433d96d9f53b450162194d8a95c8abe1310a6caf
Roman Danilevskij <fecc_@mail.ru>
2022-12-25 22:40:54 +0300

    Вносит правки

--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,4 @@
-function myFunction() {
+/*function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
@@ -15,15 +15,29 @@ window.onclick = function(event) {
     }
   }
 }
+*/
 
+let nav = document.querySelector('.nav__list');
 let navToggle = document.querySelector('.nav-toggle');
 
+nav.classList.remove('nav--nojs');
+
 navToggle.addEventListener('click', function () {
-  if (navToggle.classList.contains('nav-toggle--close')) {
-    navToggle.classList.remove('nav-toggle--close');
+  if (nav.classList.contains('nav__list--closed')) {
+    nav.classList.remove('nav__list--closed');
+    nav.classList.add('nav__list--opened');
+  } else {
+    nav.classList.add('nav__list--closed');
+    nav.classList.remove('nav__list--opened');
+  }
+});
+
+navToggle.addEventListener('click', function () {
+  if (navToggle.classList.contains('nav-toggle--closed')) {
+    navToggle.classList.remove('nav-toggle--closed');
     navToggle.classList.add('nav-toggle--opened');
   } else {
-    navToggle.classList.add('nav-toggle--close');
+    navToggle.classList.add('nav-toggle--closed');
     navToggle.classList.remove('nav-toggle--opened');
   }
 });
