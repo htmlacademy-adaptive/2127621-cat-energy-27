1d72e3ba667dad440246e5e08dae1d47949437d8
Keks <keks@htmlacademy.ru>
2022-12-27 23:39:23 +0300

    :heavy_check_mark: Сборка #9

--- 9/js/script.js
+++ 9/js/script.js
@@ -4,11 +4,11 @@ let navToggle = document.querySelector('.nav-toggle');
 nav.classList.remove('nav--nojs');
 
 navToggle.addEventListener('click', function () {
-  if (nav.classList.contains('nav__list--closed')) {
-    nav.classList.remove('nav__list--closed');
+  if (nav.classList.contains('nav--closed')) {
+    nav.classList.remove('nav--closed');
     nav.classList.add('nav__list--opened');
   } else {
-    nav.classList.add('nav__list--closed');
+    nav.classList.add('nav--closed');
     nav.classList.remove('nav__list--opened');
   }
   if (navToggle.classList.contains('nav-toggle--closed')) {
