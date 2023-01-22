04b00749212c26189078cd422f4c9b12082bbd68
Roman Danilevskij <fecc_@mail.ru>
2023-01-22 23:45:37 +0300

    Правки в ДЗ 9

--- source/js/slider.js
+++
@@ -1,4 +0,0 @@
-let container = document.querySelector(".example__slider");
-document.querySelector(".example__slider-range").addEventListener("input", (e) => {
-  container.style.setProperty("--position", `${e.target.value}%`);
-});
