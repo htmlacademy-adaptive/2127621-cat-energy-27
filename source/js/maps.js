1a6350d369175307e1462cd3154912e4aa94b3bc
Roman Danilevskij <fecc_@mail.ru>
2023-01-24 17:30:41 +0300

    Правки после первой защиты

--- source/js/maps.js
+++ source/js/maps.js
@@ -1,5 +1,5 @@
-let maps = document.querySelector('.contacts__link');
+//let maps = document.querySelector('.contacts__link');
 let interactivemaps = document.querySelector('.contacts__maps');
 
 interactivemaps.classList.remove("maps-no-js");
-maps.classList.add("maps-no-js");
+//maps.classList.add("maps-no-js");
